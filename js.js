8bcbffb8d67123dbcd6602a5efa76fbfc80286fd
InharLlona <inharllona@gmail.com>
2021-12-14 10:45:05 +0100

    Reserva con actualización automática

--- js.js
+++ js.js
@@ -1,87 +1,61 @@
 
-var primerav =1;
-var ResultadoOP = 0;
-document.getElementById("idPrimer-numero").addEventListener("keyup",asignarValorUno);
+var numNoches,numParking = 0;
+document.getElementById("idTipo").addEventListener("change",asignarValores);
+document.getElementById("idTamano").addEventListener("change",asignarValores);
+document.getElementById("idSpa").addEventListener("click",asignarValores);
+document.getElementById("idNumNoches").addEventListener("keyup",asignarValores);
+document.getElementById("idNumParking").addEventListener("keyup",asignarValores);
 
-function asignarValorUno () {
-    if (primerav){
-    a = parseFloat(document.getElementById("idPrimer-numero").value);
-    if (isNaN(a)){
-        document.getElementById("idResultado").value ="Err";
-        asignarValorUno();
-    } 
+function asignarValores () {
+    tipo = document.getElementById("idTipo").value;
+    uso = document.getElementById("idTamano").value;
+    spa=document.getElementById("idSpa").checked;
+    numNoches=parseFloat(document.getElementById("idNumNoches").value);
+    numParking=parseFloat(document.getElementById("idNumParking").value);
+    calcularPrecio();
+}
+
+var precio = {
+   Standar: 100,
+   JuniorSuite:120,
+   Suite:150,
+}
+
+function calcularPrecio (){
+    numNoches = (isNaN(numNoches))?0:numNoches;
+    numParking = (isNaN(numParking))?0:numNoches;
+   
+    if (spa){
+        precioS = 20;
+        mensajeSpa1 = " con Spa";
+        mensajeSpa2 = 10*numNoches + "Spa";
+        nocheSpa= numNoches;
     }else{
-    b = parseFloat(document.getElementById("idPrimer-numero").value);
+        precioS = nocheSpa = 0; 
+        mensajeSpa1 = " sin Spa";
+        mensajeSpa2 = "";
     }
-}
-
-document.getElementById("idSuma").addEventListener("click",()=>calcular("suma"))
-document.getElementById("idResta").addEventListener("click",()=>calcular('resta'))
-document.getElementById("idMultiplicacion").addEventListener("click",()=>calcular('multiplicacion'))
-document.getElementById("idDivision").addEventListener("click",()=>calcular('division'))
-document.getElementById("IdIgual").addEventListener("click",()=>calcular('mostrar'))
-
-var op1,op2;
-var symbolo,sResultado,msResultado;
-
-var calcular = (operacion) =>{
-    document.getElementById("idPrimer-numero").value = "";
-    opeeje = op1;
-    op1 = operacion;
-if (primerav == 1){
-    display (false);    
-    }else{  calculara (opeeje,b); }  
-}
-
-var calculara = (operaciona,numa) =>{
-   switch (operaciona)
-        {
-    case 'suma':
-         ResultadoOP = ResultadoOP + numa;
-         symbolo = "+";
-         break;
-    case "resta":  
-         ResultadoOP = ResultadoOP - numa;
-         symbolo = "-";
-         break;
-    case "multiplicacion": 
-        ResultadoOP = numa * ResultadoOP;
-        symbolo = "*";
-         break;
-    case "division":  
-         ResultadoOP=ResultadoOP/numa;
-         symbolo = "/";
-         break;
-        }
-   if (op1 == "mostrar"){ 
-    display(true); 
-   }else{
-   display(false);
+    switch (uso){
+       case "individual":
+            precionoche = ((precio[tipo] + precioS) - ((precio[tipo] + precioS)*0.25));
+            precioT = ((precio[tipo] + precioS) - ((precio[tipo] + precioS)*0.25)) * numNoches + (10 * numParking) ;
+            mensajeuso=", al 75% por uso individual: ";
+            break;
+        case "doble":
+            precionoche = (precio[tipo] + precioS);
+            precioT = (precio[tipo] + precioS) * numNoches + (10 * numParking) ;
+            mensajeuso=": ";
+            break;
+        case "triple":
+            precionoche = ((precio[tipo] + precioS) + ((precio[tipo] + precioS)*0.25));
+            precioT = ((precio[tipo] + precioS) + ((precio[tipo] + precioS)*0.25)) * numNoches + (10 * numParking) ;
+            mensajeuso=", al 125% por uso individual: ";
+            break;
    }
+   precioT = (numNoches==0)?0:precioT;
+   document.getElementById("idPrecio").value =precioT + "€";
+   document.getElementById("idPrecioTexto").innerHTML = numNoches +" " + "noches" + "x" +" "+ "Hab."+tipo+ mensajeSpa1 +" y "+ numParking + " noches de parking. En este caso la tarifa por noche sería: "+(precio[tipo])+"€ noche " + "+ " + nocheSpa*10 +" € spa" + mensajeuso + precionoche + "€/noche x " +numNoches+"noches "+ "(10€ parking x " +numParking +" noches) = " + precioT; 
 }
-
-var display = (rst) =>{
-    if (primerav){
-        document.getElementById("idResultado").value =a;
-        ResultadoOP,msResultado = a;
-        primerav =0;
-        }else{
-            if(rst==true){
-                a,b,opeeje,op1,msResultado="";
-                document.getElementById("idResultado").value =ResultadoOP;
-                primerav =1;
-            }else{
-                msResultado= msResultado +symbolo +b;
-                document.getElementById("idResultado").value =msResultado + "=" + ResultadoOP;
-            }
-        }
-}
-var isNaNI = (uno) => ( isNaN(uno))?true:false;
-
-
-
-
-
 
 
 
